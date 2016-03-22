b09ffb22bd58ef163a1bc8a8d90c0779c96bf654
Florian Sey <florian.sey@gmail.com>
2016-03-22 18:29:43 +0100

    Handle JSON file with BOM.

--- test/test.js
+++ test/test.js
@@ -13,54 +13,49 @@ var utils = require('../lib/utils');
 
 describe('#JSONFilesToMap', function() {
   it('should return an empty array if no arguments is passed', function() {
-    // when
     var results = utils.JSONFilesToMap();
 
-    // then
     assert.deepEqual(results, []);
   });
 
   it('should return a correct object', function() {
-    // given
     var sources = ['test/fixtures/fileA.json'];
 
-    // when
     var results = utils.JSONFilesToMap(sources);
 
-    // then
     assert.ok(results.length > 0);
     assert.strictEqual(results[0].fileName, 'test/fixtures/fileA.json');
     assert.strictEqual(results[0].parsedJSON['JSON.string.1'], 'string1');
+  });
+
+  it('should handle file with BOM', function () {
+    var sources = ['test/fixtures/fileWithBOM.json'];
+    var results = utils.JSONFilesToMap(sources);
+    assert.ok(results.length > 0);
   });
 });
 
 
 describe('#compareMaps', function() {
   it('should return an empty array if no arguments is passed', function() {
-    // when
     var reports = utils.compareMaps();
 
-    // then
     assert.deepEqual(reports, []);
   });
 
   it('should return an empty array if no differences are found', function() {
-    // given
     var values = {"a": "a", "b": "b"};
     var maps = [
       {parsedJSON: values, fileName: 'fileA'},
       {parsedJSON: values, fileName: 'fileB'}
     ];
 
-    // when
     var reports = utils.compareMaps(maps);
 
-    // then
     assert.deepEqual(reports, []);
   });
 
   it('should return the missing key if some differences are found', function() {
-    // given
     var valueA = {"a": "a", "b": "b"};
     var valueB = {"b": "b"};
     var maps = [
@@ -68,16 +63,13 @@ describe('#compareMaps', function() {
       {parsedJSON: valueB, fileName: 'fileB'}
     ];
 
-    // when
     var reports = utils.compareMaps(maps);
 
-    // then
     assert.equal(reports.length, 1);
   });
 
 
   it('should return the missing key if some differences are found inside of sub-objects', function() {
-    // given
     var valueA = {"a": {"me":"a","mo":"a"}, "b": "b"};
     var valueB = {"a": {"me":"a"}, "b": "b"};
     var maps = [
@@ -85,10 +77,8 @@ describe('#compareMaps', function() {
       {parsedJSON: valueB, fileName: 'fileB'}
     ];
 
-    // when
     var reports = utils.compareMaps(maps);
 
-    // then
     assert.equal(reports.length, 1);
     assert.equal(reports[0].key, 'a.mo');
   });
